b96b75195072171338f872d1ba8c07ecc1952d5b
Wellinator <wellcoj@gmail.com>
2022-06-09 17:50:15 -0300

    feat: allow loading assets from cwd

--- src/engine/include/modules/file_manager.hpp
+++ src/engine/include/modules/file_manager.hpp
@@ -1,7 +1,7 @@
 /*
 # ______       ____   ___
-#   |     \/   ____| |___|    
-#   |     |   |   \  |   |       
+#   |     \/   ____| |___|
+#   |     |   |   \  |   |
 #-----------------------------------------------------------------------
 # Copyright 2020, tyra - https://github.com/h4570/tyra
 # Licenced under Apache License 2.0
@@ -14,11 +14,15 @@
 #include <tamtypes.h>
 #include <cstdio>
 #include <kernel.h>
+#include <limits.h>
+#include <unistd.h>
+#include <cstring>
+
 #include "../utils/debug.hpp"
 #include "../utils/string.hpp"
 
 #ifdef NDEBUG
-#define FS_SOURCE "mass:"
+#define FS_SOURCE ""
 #else
 #define FS_SOURCE "host:"
 #endif
@@ -37,6 +41,16 @@ public:
     FILE *openFile(const char *t_subfolder, const char *t_name, const char *t_extension);
     FILE *openFile(const char *t_dir, const char *t_file);
     FILE *openFile(const char *t_path);
+    char *getBasePath();
+
+private:
+    char cwd[NAME_MAX];
+
+    // Argv name+path & just path
+    char elfName[NAME_MAX] __attribute__((aligned(16)));
+    char elfPath[NAME_MAX - 14]; // It isn't 256 because elfPath will add subpaths
+
+    void setPathInfo(char *path);
 };
 
 #endif
--- src/engine/modules/file_manager.cpp
+++ src/engine/modules/file_manager.cpp
@@ -1,15 +1,20 @@
 #include "../include/modules/file_manager.hpp"
 
 FileManager::FileManager()
-{}
+{
+    getcwd(cwd, sizeof(cwd));
+    printf("Booting from %s\n", cwd);
+    setPathInfo(cwd);
+}
 
 FileManager::~FileManager()
-{}
+{
+}
 
 FILE *FileManager::openFile(const char *t_subfolder, const char *t_name, const char *t_extension)
 {
     char *path_part1 = String::createConcatenated(t_subfolder, t_name);
-    char *path_part2 = String::createConcatenated(FS_SOURCE, path_part1);
+    char *path_part2 = String::createConcatenated(this->getBasePath(), path_part1);
     char *path = String::createConcatenated(path_part2, t_extension);
     delete[] path_part1;
     delete[] path_part2;
@@ -29,9 +34,9 @@ FILE *FileManager::openFile(const char *t_subfolder, const char *t_name, const c
 FILE *FileManager::openFile(const char *t_dir, const char *t_file)
 {
     char *path_part1 = String::createConcatenated(t_dir, t_file);
-    char *path = String::createConcatenated(FS_SOURCE, path_part1);
+    char *path = String::createConcatenated(this->getBasePath(), path_part1);
     delete[] path_part1;
-    
+
     printf("Opening file: %s\n", path);
     FILE *file = fopen(path, "rb");
 
@@ -46,7 +51,7 @@ FILE *FileManager::openFile(const char *t_dir, const char *t_file)
 
 FILE *FileManager::openFile(const char *t_path)
 {
-    char *path = String::createConcatenated(FS_SOURCE, t_path);
+    char *path = String::createConcatenated(this->getBasePath(), t_path);
 
     printf("Opening file: %s\n", path);
     FILE *file = fopen(path, "rb");
@@ -58,4 +63,39 @@ FILE *FileManager::openFile(const char *t_path)
     }
 
     return file;
+}
+
+void FileManager::setPathInfo(char *path)
+{
+    char *ptr;
+
+    strcpy(this->elfName, path);
+    strcpy(this->elfPath, path);
+
+    ptr = strrchr(this->elfPath, '/');
+    if (ptr == NULL)
+    {
+        ptr = strrchr(this->elfPath, '\\');
+        if (ptr == NULL)
+        {
+            ptr = strrchr(this->elfPath, ':');
+            if (ptr == NULL)
+            {
+                printf("Did not find path (%s)!\n", path);
+                SleepThread();
+            }
+        }
+    }
+
+    ptr++;
+    *ptr = '\0';
+
+    printf("ELF name is %s\n", this->elfName);
+    printf("path is %s\n", this->elfPath);
+}
+
+char *FileManager::getBasePath()
+{
+    char *result = FS_SOURCE;
+    return (sizeof(FS_SOURCE) > 1) ? result : cwd;
 }
