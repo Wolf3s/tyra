3f69edefa36f38045c81329e06cf11ab19f0f1fd
Wellinator <wellcoj@gmail.com>
2022-06-09 17:54:28 -0300

    refactor:  apply DRY principle

--- src/engine/modules/file_manager.cpp
+++ src/engine/modules/file_manager.cpp
@@ -14,39 +14,16 @@ FileManager::~FileManager()
 FILE *FileManager::openFile(const char *t_subfolder, const char *t_name, const char *t_extension)
 {
     char *path_part1 = String::createConcatenated(t_subfolder, t_name);
-    char *path_part2 = String::createConcatenated(this->getBasePath(), path_part1);
-    char *path = String::createConcatenated(path_part2, t_extension);
+    char *path = String::createConcatenated(path_part1, t_extension);
     delete[] path_part1;
-    delete[] path_part2;
 
-    printf("Opening file: %s\n", path);
-    FILE *file = fopen(path, "rb");
-
-    if (file == NULL)
-    {
-        char *errorMsg = String::createConcatenated("Failed to load the file: ", path);
-        assertMsg(file != NULL, errorMsg);
-    }
-
-    return file;
+    return openFile(path);
 }
 
 FILE *FileManager::openFile(const char *t_dir, const char *t_file)
 {
-    char *path_part1 = String::createConcatenated(t_dir, t_file);
-    char *path = String::createConcatenated(this->getBasePath(), path_part1);
-    delete[] path_part1;
-
-    printf("Opening file: %s\n", path);
-    FILE *file = fopen(path, "rb");
-
-    if (file == NULL)
-    {
-        char *errorMsg = String::createConcatenated("Failed to load the file: ", path);
-        assertMsg(file != NULL, errorMsg);
-    }
-
-    return file;
+    char *path = String::createConcatenated(t_dir, t_file);
+    return openFile(path);
 }
 
 FILE *FileManager::openFile(const char *t_path)
